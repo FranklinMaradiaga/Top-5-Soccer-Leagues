1bac586c25511341469da0aa1e6929fb09d51370
Franklin S Maradiaga <franklin.maradiaga@lc.cuny.edu>
2022-07-08 05:58:22 +0000

    updated main file

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ def get_url(league, season):
         "La Liga": "esp.1",
         "Premier League": "eng.1",
     }
-    url = f"https://api-football-standings.azharimm.site/leagues/{d[league]}/standings?season={season}&sort=asc"
-    return url
+    # f1 = f"https://api-football-standings.azharimm.site/leagues/{d[league]}/"
+    # f2 = "standings?season={season}&sort=asc"
+    return f"https://api-football-standings.azharimm.site/leagues/{d[league]}/standings?season={season}&sort=asc"
 
 
 def main():
